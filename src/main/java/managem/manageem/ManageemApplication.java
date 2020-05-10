7b9c474a7f6628046ca0a3f2ef9205221aae1e22
srinath <srinath.mahagama@itechro.com>
2020-05-10 12:13:24 +0530

    Initial Commit

---
+++ src/main/java/managem/manageem/ManageemApplication.java
@@ -0,0 +1,13 @@
+package managem.manageem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageemApplication.class, args);
+    }
+
+}
